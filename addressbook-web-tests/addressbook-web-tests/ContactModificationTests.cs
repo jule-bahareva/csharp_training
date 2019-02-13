d98a8be4766529c248be91447f3dbf09af42b06b
yuliya.bahareva <yuliya.bahareva@rokolabs.com>
2019-02-13 23:12:02 +0400

    Optimization - type method

--- addressbook-web-tests/addressbook-web-tests/ContactModificationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/ContactModificationTests.cs
@@ -13,8 +13,24 @@ namespace WebAddressbookTests
         public void ContactModificationTest()
         {
             ContactData newContact = new ContactData("Edited_Firstname", "Edited_Lastname");
-
-            app.Contacts.Modify(1,newContact);
+            newContact.Middlename = null;
+            newContact.Nickname = null;
+            newContact.Title = null;
+            newContact.Company = null;
+            newContact.Address = null;
+            newContact.Home = null;
+            newContact.Mobile = null;
+            newContact.Work = null;
+            newContact.Fax = null;
+            newContact.Email = null;
+            newContact.Email2 = null;
+            newContact.Email3 = null;
+            newContact.Homepage = null;
+            newContact.Address2 = null;
+            newContact.Phone2 = null;
+            newContact.Notes = null;
+             
+           app.Contacts.Modify(1,newContact);
 
         }
     }
