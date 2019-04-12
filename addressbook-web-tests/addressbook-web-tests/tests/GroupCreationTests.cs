b6ce9bea7e08967bdaa6e1cd8ddad454340e4595
yuliya.bahareva <yuliya.bahareva@rokolabs.com>
2019-04-12 19:12:52 +0400

    data generator for groups csv

--- addressbook-web-tests/addressbook-web-tests/tests/GroupCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/GroupCreationTests.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
+using System.IO;
 using System.Collections.Generic;
 using NUnit.Framework;
 
@@ -27,8 +28,26 @@ namespace WebAddressbookTests
             return groups; 
         }
 
+        public static IEnumerable<GroupData> GroupDataFromFile()
+        {
+            List<GroupData> groups = new List<GroupData>();
+            string[] lines = File.ReadAllLines(@"groups.csv");
 
-        [Test, TestCaseSource("RandomGroupDataProvider")]
+            foreach( string l in lines)
+            {
+                string[] parts = l.Split(',');
+                groups.Add(new GroupData(parts[0])
+                { Header = parts[1],
+                  Footer = parts[2],
+      
+                });
+
+            }
+
+            return groups;
+        }
+
+        [Test, TestCaseSource("GroupDataFromFile")]
         public void GroupCreationTest(GroupData group)
         {
 
--- addressbook-web-tests/addressbook-web-tests/tests/TestBase.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/TestBase.cs
@@ -20,5 +20,21 @@ namespace WebAddressbookTests
 
         }
 
+        public static Random rnd = new Random();
+
+        public static string GenerateRandomString(int max)
+        {
+
+            int l = Convert.ToInt32(rnd.NextDouble() * max);
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < l; i++)
+            {
+                builder.Append(Convert.ToChar(32 + Convert.ToInt32(rnd.NextDouble() *65)));
+            }
+
+            return builder.ToString();
+
+        }
+
     }
 }
