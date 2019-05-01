aea6f089cde2edcf9656b6798ef110ea16e4264f
yuliya.bahareva <yuliya.bahareva@rokolabs.com>
2019-05-01 20:11:28 +0400

    write xml data and use xml data in tests

--- addressbook-web-tests/addressbook-test-data-generators/Program.cs
+++ addressbook-web-tests/addressbook-test-data-generators/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WebAddressbookTests;
+using System.Xml;
+using System.Xml.Serialization;
 using System.IO;
 
 namespace addressbook_test_data_generators
@@ -14,17 +16,53 @@ namespace addressbook_test_data_generators
         {
             int count = Convert.ToInt32(args[0]);
             StreamWriter writer = new StreamWriter(args[1]);
-            
+            string format = args[2];
 
-            for (int i =0; i<count; i++ )
+            List<GroupData> groups = new List<GroupData>();
+
+            for (int i = 0; i < count; i++ )
             {
-                writer.WriteLine(String.Format("${0},{1},{2}",
-                    TestBase.GenerateRandomString(10),
-                    TestBase.GenerateRandomString(10),
-                    TestBase.GenerateRandomString(10)));
+                groups.Add(new GroupData(TestBase.GenerateRandomString(10))
+                {
+                    Header = TestBase.GenerateRandomString(10),
+                    Footer = TestBase.GenerateRandomString(10)
+
+                });
+
             }
-            writer.Close();
+
+            if (format == "csv")
+            {
+                writeGroupsToCsvFile(groups, writer);
+            }
+            else if (format == "xml")
+            {
+                writeGroupsToXmlFile(groups, writer);
+            }
+            else
+            {
+                System.Console.Out.Write("Unrecognized format" + format);
+            }
+            
+            writer.Close(); 
 
         }
+
+        static void writeGroupsToCsvFile(List<GroupData> groups, StreamWriter writer)
+        {
+            foreach ( GroupData group in groups)
+            {
+                writer.WriteLine(String.Format("${0},{1},{2}",
+                    group.Name, group.Header, group.Footer));
+            }
+
+        }
+
+        static void writeGroupsToXmlFile(List<GroupData> groups, StreamWriter writer)
+        {
+            new XmlSerializer(typeof(List<GroupData>)).Serialize(writer, groups);
+        }
+
+
     }
 }
--- addressbook-web-tests/addressbook-web-tests/tests/GroupCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/GroupCreationTests.cs
@@ -5,6 +5,8 @@ using System.Threading;
 using System.IO;
 using System.Collections.Generic;
 using NUnit.Framework;
+using System.Xml;
+using System.Xml.Serialization;
 
 
 
@@ -28,7 +30,7 @@ namespace WebAddressbookTests
             return groups; 
         }
 
-        public static IEnumerable<GroupData> GroupDataFromFile()
+        public static IEnumerable<GroupData> GroupDataFromCsvFile()
         {
             List<GroupData> groups = new List<GroupData>();
             string[] lines = File.ReadAllLines(@"groups.csv");
@@ -39,15 +41,25 @@ namespace WebAddressbookTests
                 groups.Add(new GroupData(parts[0])
                 { Header = parts[1],
                   Footer = parts[2],
-      
-                });
-
+                });  
             }
-
+ 
             return groups;
         }
 
-        [Test, TestCaseSource("GroupDataFromFile")]
+
+        public static IEnumerable<GroupData> GroupDataFromXmlFile()
+        {
+ 
+
+            return (List<GroupData>) new XmlSerializer(typeof(List<GroupData>))
+                .Deserialize(new StreamReader(@"groups.xml"));
+
+     
+        }
+
+
+        [Test, TestCaseSource("GroupDataFromXmlFile")]
         public void GroupCreationTest(GroupData group)
         {
 
